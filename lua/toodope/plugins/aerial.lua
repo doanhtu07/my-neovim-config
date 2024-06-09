096869c17b807d3d5a25d18e69934732b636bedb
doanhtu07 <doanhtu07@gmail.com>
2024-06-09 14:23:11 -0500

    Update plugins + Change aerial and lualine config

--- lua/toodope/plugins/aerial.lua
+++ lua/toodope/plugins/aerial.lua
@@ -2,12 +2,20 @@ return {
 	"stevearc/aerial.nvim",
 	config = function()
 		require("aerial").setup({
+			backends = { "lsp", "treesitter", "markdown", "asciidoc", "man" },
+
+			layout = {
+				min_width = 40,
+			},
+
 			-- optionally use on_attach to set keymaps when aerial has attached to a buffer
 			on_attach = function(bufnr)
 				-- Jump forwards/backwards with '{' and '}'
 				vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
 				vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
 			end,
+
+			filter_kind = false,
 		})
 		-- You probably also want to set a keymap to toggle aerial
 		vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
--- lua/toodope/plugins/nvim-tree.lua
+++ lua/toodope/plugins/nvim-tree.lua
@@ -5,6 +5,16 @@ return {
 		local nvimtree = require("nvim-tree")
 		local api = require("nvim-tree.api")
 
+		-- https://www.reddit.com/r/neovim/comments/wvcz64/nvimtreelua_how_to_center_floating_window/
+		local gheight = vim.api.nvim_list_uis()[1].height
+		local gwidth = vim.api.nvim_list_uis()[1].width
+		local width = 30
+		local height = 30
+
+		-- https://github.com/MarioCarrion/videos/blob/269956e913b76e6bb4ed790e4b5d25255cb1db4f/2023/01/nvim/lua/plugins/nvim-tree.lua
+		local HEIGHT_RATIO = 0.8 -- You can change this
+		local WIDTH_RATIO = 0.5 -- You can change this too
+
 		-- recommended settings from nvim-tree documentation
 		vim.g.loaded_netrw = 1
 		vim.g.loaded_netrwPlugin = 1
@@ -14,8 +24,31 @@ return {
 				cmd = "trash",
 			},
 			view = {
-				width = 35,
+				width = function()
+					return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+				end,
 				relativenumber = true,
+				float = {
+					enable = true,
+					open_win_config = function()
+						local screen_w = vim.opt.columns:get()
+						local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+						local window_w = screen_w * WIDTH_RATIO
+						local window_h = screen_h * HEIGHT_RATIO
+						local window_w_int = math.floor(window_w)
+						local window_h_int = math.floor(window_h)
+						local center_x = (screen_w - window_w) / 2
+						local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+						return {
+							border = "rounded",
+							relative = "editor",
+							row = center_y,
+							col = center_x,
+							width = window_w_int,
+							height = window_h_int,
+						}
+					end,
+				},
 			},
 			-- change folder arrow icons
 			renderer = {
