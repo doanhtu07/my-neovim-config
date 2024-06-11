077c3d725b1cafa92bf01a109655d8fa2834af1d
doanhtu07 <doanhtu07@gmail.com>
2024-06-10 23:03:33 -0500

    Update plugins

--- lua/toodope/plugins/telescope.lua
+++ lua/toodope/plugins/telescope.lua
@@ -15,7 +15,7 @@ return {
 
 		telescope.setup({
 			defaults = {
-				path_display = { "smart" },
+				path_display = { "truncate" },
 				mappings = {
 					i = {
 						["<C-k>"] = actions.move_selection_previous, -- move to prev result
