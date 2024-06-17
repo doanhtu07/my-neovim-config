8c64a65233c435b711ab813e1290843dd2402618
doanhtu07 <doanhtu07@gmail.com>
2024-06-17 00:54:32 -0500

    Update plugins and add local leader

--- lua/toodope/plugins/neorg.lua
+++ lua/toodope/plugins/neorg.lua
@@ -14,13 +14,13 @@ return {
 				load = {
 					["core.defaults"] = {}, -- Loads default behaviour
 					["core.concealer"] = {}, -- Adds pretty icons to your documents
-					-- ["core.dirman"] = { -- Manages Neorg workspaces
-					-- 	config = {
-					-- 		workspaces = {
-					-- 			notes = "~/notes",
-					-- 		},
-					-- 	},
-					-- },
+					["core.dirman"] = { -- Manages Neorg workspaces
+						config = {
+							workspaces = {
+								notes = "~/Desktop/Private/notes/",
+							},
+						},
+					},
 				},
 			})
 		end,
--- lua/toodope/plugins/which-key.lua
+++ lua/toodope/plugins/which-key.lua
@@ -1,17 +1,29 @@
 return {
-  "folke/which-key.nvim", -- Easier for us to remember the keymap we config
-  event = "VeryLazy", -- for Lazyvim to know that it's not very important for the UI and can load later
+	"folke/which-key.nvim", -- Easier for us to remember the keymap we config
+	event = "VeryLazy", -- for Lazyvim to know that it's not very important for the UI and can load later
 
-  -- Run during Neovim startup instead of when the plugin actually loads
-  init = function()
-    -- how long which-key will wait before activates
-    vim.o.timeout = true
-    vim.o.timeoutlen = 500
-  end,
+	-- Run during Neovim startup instead of when the plugin actually loads
+	init = function()
+		-- how long which-key will wait before activates
+		vim.o.timeout = true
+		vim.o.timeoutlen = 500
+	end,
 
-  -- We could call which-key setup() but if define opts, it will do automatically
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use default
-  },
+	config = function()
+		local whichkey = require("which-key")
+		whichkey.setup({})
+
+		-- this is to fix bug: https://github.com/folke/which-key.nvim/issues/476
+		-- https://github.com/folke/which-key.nvim/issues/476
+		vim.api.nvim_create_autocmd("FileType", {
+			desc = "Set up neorg Which-Key descriptions",
+			group = vim.api.nvim_create_augroup("neorg_mapping_descriptions", { clear = true }),
+			pattern = "norg",
+			callback = function()
+				vim.keymap.set("n", "<localleader>", function()
+					whichkey.show("\\")
+				end, { buffer = true })
+			end,
+		})
+	end,
 }
