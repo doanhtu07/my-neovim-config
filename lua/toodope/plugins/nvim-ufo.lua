a453c51783899cc1294dbc6038f8439f18bc7093
doanhtu07 <doanhtu07@gmail.com>
2024-06-19 11:15:52 -0500

    Add virtual text to nvim-ufo

--- lua/toodope/plugins/nvim-ufo.lua
+++ lua/toodope/plugins/nvim-ufo.lua
@@ -12,6 +12,45 @@ return {
 		vim.keymap.set("n", "zR", require("ufo").openAllFolds)
 		vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
 
-		require("ufo").setup()
+		-- Add virtual text to show how many lines a fold is
+		local handler = function(virtText, lnum, endLnum, width, truncate)
+			local newVirtText = {}
+			local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+
+			local sufWidth = vim.fn.strdisplaywidth(suffix)
+			-- imagine whole row consists of many cells, we want to leave the last cell to suffix, so everything else in virtText should be squeezed into targetWidth
+			local targetWidth = width - sufWidth
+			local curWidth = 0
+
+			for _, chunk in ipairs(virtText) do
+				local chunkText = chunk[1]
+				local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+
+				if targetWidth > curWidth + chunkWidth then
+					table.insert(newVirtText, chunk)
+				else
+					chunkText = truncate(chunkText, targetWidth - curWidth)
+					local hlGroup = chunk[2]
+					table.insert(newVirtText, { chunkText, hlGroup })
+
+					chunkWidth = vim.fn.strdisplaywidth(chunkText)
+					-- str width returned from truncate() may less than 2nd argument, need padding
+					if curWidth + chunkWidth < targetWidth then
+						suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+					end
+
+					break
+				end
+
+				curWidth = curWidth + chunkWidth
+			end
+
+			table.insert(newVirtText, { suffix, "MoreMsg" })
+			return newVirtText
+		end
+
+		require("ufo").setup({
+			fold_virt_text_handler = handler,
+		})
 	end,
 }
