aabfbd639f28f94c1fb5f0af7d47440845598487
doanhtu07 <doanhtu07@gmail.com>
2024-06-28 15:35:04 -0500

    Update keymap for Aerial

--- lua/toodope/plugins/aerial.lua
+++ lua/toodope/plugins/aerial.lua
@@ -10,9 +10,8 @@ return {
 
 			-- optionally use on_attach to set keymaps when aerial has attached to a buffer
 			on_attach = function(bufnr)
-				-- Jump forwards/backwards with '{' and '}'
-				vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
-				vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+				vim.keymap.set("n", "[a", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+				vim.keymap.set("n", "]a", "<cmd>AerialNext<CR>", { buffer = bufnr })
 			end,
 
 			filter_kind = false,
