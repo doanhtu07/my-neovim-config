656ecd7c1169c390f84077b01ba707686d74d236
doanhtu07 <doanhtu07@gmail.com>
2024-07-13 03:24:49 -0500

    Fix jdtls slow start up on standalone java files

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -2,6 +2,11 @@
 local config = {
 	-- https://github.com/mfussenegger/nvim-jdtls/discussions/567
 	cmd = { vim.fn.expand("~/.local/share/nvim/mason/bin/jdtls") }, -- See `:h base-directories`
-	root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
+	root_dir = vim.fs.dirname(vim.fs.find({
+		".git",
+		"toodopejava", -- For faster search. Since jdtls will search upward, so for standalone java files, jdtls ends up search the whole home directory...
+		"mvnw",
+		"gradlew",
+	}, { upward = true })[1]),
 }
 require("jdtls").start_or_attach(config)
