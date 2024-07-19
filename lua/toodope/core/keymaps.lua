f0d7dc10692c8cf567879ae5fd2d3c0ca246522b
doanhtu07 <doanhtu07@gmail.com>
2024-07-18 21:23:41 -0500

    Update plugins

--- lua/toodope/core/keymaps.lua
+++ lua/toodope/core/keymaps.lua
@@ -27,9 +27,9 @@ keymap.set("n", "<leader>s<", "<C-w>10<", { desc = "Decrease window width" })
 -- tab management
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+keymap.set("n", "L", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+keymap.set("n", "H", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 
 -- text wrap
 keymap.set("n", "<leader>tw", function()
