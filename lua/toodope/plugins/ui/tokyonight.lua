4ece2f4a8983dd4f2c2e4f1309bff5ed8bdfef62
doanhtu07 <doanhtu07@gmail.com>
2024-08-14 08:51:10 -0500

    Update plugins

--- lua/toodope/plugins/ui/tokyonight.lua
+++ lua/toodope/plugins/ui/tokyonight.lua
@@ -6,7 +6,7 @@ return {
 
 		-- https://github.com/folke/tokyonight.nvim/blob/main/extras/lua/tokyonight_night.lua
 		tokyonight.setup({
-			style = "night",
+			style = "moon",
 			on_highlights = function(hl, colors)
 				-- Customize line number color
 				hl.LineNrAbove = {
